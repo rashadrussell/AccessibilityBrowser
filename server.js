653b1ca6d9cca490a8be06962c145dbee5c78eea
Rashad Russell <rashad.j.russell@gmail.com>
2013-09-07 10:40:09 -0500

    Submit inital project

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+  , http    = require('http')
+  , path    = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+ 
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
